10b7ea0684ef8368eca25bfbdfe756df4bf374fd
Herly Olivares <herlyarge@gmail.com>
2015-11-16 13:43:37 -0430

    Commit inicial

---
+++ database/migrations/2015_11_13_204731_create_categorias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+           $table->increments('id');
+            $table->string('name', 255)->unique();
+            $table->string('slug');
+            $table->text('description');
+            $table->string('color', 30);
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categorias');
+    }
+}
